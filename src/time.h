cadefd455506298780f750522ed4b638f338b654
jimjibone <jamesreuss@gmail.com>
2013-03-27 14:28:46 +0000

    add offset to auto time

--- src/time.h
+++ src/time.h
@@ -24,9 +24,9 @@ void time_dec_hour(void);
 
 #ifdef AUTO_TIME
 	// initialise with compile-time time with an offset to account for build/upload time
-	_time time = {HOURS,MINUTES,SECONDS};
+	_time time = {HOURS,MINUTES,SECONDS+9};
 #else
-	_time time = {9,0,0};
+	_time time = {0,0,0};
 #endif
 
 
